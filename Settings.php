9caa7f1c1c9e8cddd88a37f2b25a505154779420
koroshiya <korocresent@gmail.com>
2014-02-28 15:50:33 +1100

    Modified constant declarations in Settings.php to remove warnings for newer versions of PHP

--- Settings.php
+++ Settings.php
@@ -2,16 +2,16 @@
 
 if (!defined('MPFM_INDEX')){die('You must access this through the root index!');}
 
-DEFINE(host, 'localhost');
-DEFINE(dbUser, 'root');
-DEFINE(dbPass, 'toor');
-DEFINE(dbName, 'Mover');
+DEFINE('host', 'localhost');
+DEFINE('dbUser', 'root');
+DEFINE('dbPass', 'toor');
+DEFINE('dbName', 'Mover');
 
-DEFINE(loginRequired, true);
-DEFINE(displayFolders, true);
-DEFINE(showFullDirectoryPaths, false); //If false, display subsection. eg. /home/user/mphpfm/target/ becomes mphpfm/target/
+DEFINE('loginRequired', true);
+DEFINE('displayFolders', true);
+DEFINE('showFullDirectoryPaths', false); //If false, display subsection. eg. /home/user/mphpfm/target/ becomes mphpfm/target/
 
-DEFINE(supportedFormats, 'zip;rar;html');
+DEFINE('supportedFormats', 'zip;rar;html');
 /*
 	Semi-colon separated list of formats of files to search for in source directory.
 	Retrieved via: preg_split('/;/', supportedFormats, -1, PREG_SPLIT_NO_EMPTY);
@@ -19,8 +19,8 @@ DEFINE(supportedFormats, 'zip;rar;html');
 	Thereafter, $formats is an array of permitted extensions.
 */
 
-DEFINE(basedir, dirname(__FILE__) . '/'); //source directory; contains files to be manipulated
-DEFINE(dest, basedir . 'workspace/'); //default target directory; target of copy, move, etc. operations
-DEFINE(uploaddir, '/tmp/');
+DEFINE('basedir', dirname(__FILE__) . '/'); //source directory; contains files to be manipulated
+DEFINE('dest', basedir . 'workspace/'); //default target directory; target of copy, move, etc. operations
+DEFINE('uploaddir', '/tmp/');
 
 ?>
